d81ce9e1adabc367a9d2da8c2a3719eb971cd9e2
guilleeh <guilleeh@uci.edu>
2019-05-05 16:20:36 -0700

    add react stripe elements

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+// const stripe = require("stripe")("sk_test_JTcthxlTgnrPaqHIOkOJeWxX00tNmpflcm");
 
 const app = express();
 
@@ -24,23 +25,25 @@ const ads = [
   {title: 'Hello, world (again)!'}
 ];
 
+const stripe = require("stripe")("sk_test_JTcthxlTgnrPaqHIOkOJeWxX00tNmpflcm");
+
+(async () => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 999,
+    currency: 'usd',
+    payment_method_types: ['card'],
+    receipt_email: 'jenny.rosen@example.com',
+  });
+  console.log(paymentIntent);
+})();
+
 app.get("/", function(req, res) {
   //when we get an http get request to the root/homepage
   res.send(ads);
 });
 
 app.get("/payment", function(req, res) {
-  var stripe = Stripe('pk_test_mVIHxjDBueW9FOhHUrp3uD7d0042aj7bq6');
-  var elements = stripe.elements();
-
-  var card = elements.create('card');
-  card.mount('#card-element');
-
-  var promise = stripe.createToken(card);
-  promise.then(function(result) {
-    // result.token is the card token.
-  });
-})
+});
 
 app.listen(PORT, () => {
   console.log('listening on port 3000');
