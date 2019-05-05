4cc6b2c05ae0b68834906c623e406325c4b3676c
guilleeh <guilleeh@uci.edu>
2019-05-05 16:28:04 -0700

    ready

--- index.js
+++ index.js
@@ -4,46 +4,25 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
-// const stripe = require("stripe")("sk_test_JTcthxlTgnrPaqHIOkOJeWxX00tNmpflcm");
+const stripe = require("stripe")("sk_test_JTcthxlTgnrPaqHIOkOJeWxX00tNmpflcm");
 
 const app = express();
 
 app.use(express.json());
 // adding Helmet to enhance your API's security
 app.use(helmet());
-
 // using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
-
 // enabling CORS for all requests
 app.use(cors());
-
 // adding morgan to log HTTP requests
 app.use(morgan('combined'));
 
-const ads = [
-  {title: 'Hello, world (again)!'}
-];
-
-const stripe = require("stripe")("sk_test_JTcthxlTgnrPaqHIOkOJeWxX00tNmpflcm");
-
-(async () => {
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: 999,
-    currency: 'usd',
-    payment_method_types: ['card'],
-    receipt_email: 'jenny.rosen@example.com',
-  });
-  console.log(paymentIntent);
-})();
-
 app.get("/", function(req, res) {
   //when we get an http get request to the root/homepage
-  res.send(ads);
+  res.send("Hello World!");
 });
 
-app.get("/payment", function(req, res) {
-});
 
 app.listen(PORT, () => {
   console.log('listening on port 3000');
