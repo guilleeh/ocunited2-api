0cd71af73ea830dc58590d5bfcd51ae8d8ddc3f0
guilleeh <guilleeh@uci.edu>
2019-05-05 15:54:41 -0700

    add stripe

--- index.js
+++ index.js
@@ -24,16 +24,23 @@ const ads = [
   {title: 'Hello, world (again)!'}
 ];
 
-
-const courses = [
-  { id: 1, name: "Algorithms" },
-  { id: 2, name: "Software Engineering" },
-  { id: 3, name: "Human Computer Interaction" }
-];
 app.get("/", function(req, res) {
   //when we get an http get request to the root/homepage
   res.send(ads);
 });
+
+app.get("/payment", function(req, res) {
+  var stripe = Stripe('pk_test_mVIHxjDBueW9FOhHUrp3uD7d0042aj7bq6');
+  var elements = stripe.elements();
+
+  var card = elements.create('card');
+  card.mount('#card-element');
+
+  var promise = stripe.createToken(card);
+  promise.then(function(result) {
+    // result.token is the card token.
+  });
+})
 
 app.listen(PORT, () => {
   console.log('listening on port 3000');
